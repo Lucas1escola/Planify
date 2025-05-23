956c7fde471be95e52930aef022d45285e2d32ff
Luana-escola <luana.herthal@escola.pr.gov.br>
2025-05-23 16:47:52 -0300

    new

--- b.js
+++ b.js
@@ -1,15 +1,54 @@
-cadastro = [
-    nome = ["Luana Herthal","Lucas Andrioli","Vinicius Ganda","Luiz Kunz","Arthur Tomelin","Raul Falcade","Eduardo dos Santos","Alana Lisbinski"],
-    email = ["luanaherhal@gmail.com", "","", "", "", "", "", ""],
-    senha = ["Luana2303", "", "", "", "", "", "", ""]
-]   
+//Armazenamento dos dados
+class Cadastro {
+    constructor(nome, email, senha, adm)
+    {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.adm = adm;
+    }
+  }
 
-function criarConta(nome, email, senha){
-    nome.push(document.getElementById(nome));
-    email.push(document.getElementById(email));
-    senha.push(document.getElementById(senha));
+pessoas = [];
+ 
+
+//Criação da conta
+function criarConta(){
+    //Recebendo os elementos a partir dos inputs do Html
+    name = document.getElementById(nome);
+    e = document.getElementById(email);
+    password = document.getElementById(senha);
+
+    //Verificando se o cadastro já existe
+    for(i = 0; i<email.length; i++){
+        if (e == email[i])
+        {
+            return alert("O email usado já está cadastrado");
+        }
+        else{
+            //Conferindo se é um de nós ou não
+            if (e == "luanaherthal@gmail.com" or
+                e == "Lucas" or
+                e =="viniciusganda2@gmail.com" or
+                e == "" or
+                e == "kunz.luizf@gmail.com" or
+                e =="alanalisbinski9@gmail.com" or
+                e == "rv.falcade@gmail.com" or
+                e == "ed22072008@gmail.com"){
+                adimin = true;
+                }
+            else{
+                adimin = false;
+            }
+
+            //Cadastrando
+            pessoa.push()
+            
+        }
+    }
 }
 
+//Login no site
 function login(email, senha){
 
 }
