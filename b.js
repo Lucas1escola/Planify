572fd863430c9eb34ea04fa9672550d7054bda80
Lucas1escola <lucas.andrioli.santos@escola.pr.gov.br>
2025-05-23 15:45:24 -0300

    Add files via upload

---
+++ b.js
@@ -0,0 +1,15 @@
+cadastro = [
+    nome = ["Luana Herthal","Lucas Andrioli","Vinicius Ganda","Luiz Kunz","Arthur Tomelin","Raul Falcade","Eduardo dos Santos","Alana Lisbinski"],
+    email = ["luanaherhal@gmail.com", "","", "", "", "", "", ""],
+    senha = ["Luana2303", "", "", "", "", "", "", ""]
+]   
+
+function criarConta(nome, email, senha){
+    nome.push(document.getElementById(nome));
+    email.push(document.getElementById(email));
+    senha.push(document.getElementById(senha));
+}
+
+function login(email, senha){
+
+}
